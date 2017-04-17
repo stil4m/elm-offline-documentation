b8d743ec3f7f9782da79751cd35973752874fc65
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-04-17 18:21:58 +0200

    initial commit

---
+++ update.js
@@ -0,0 +1,28 @@
+var request = require('sync-request');
+var _ = require('lodash');
+const fs = require('fs-extra');
+
+function fetchDoc(name) {
+    console.log('Fetching doc for', name, "...")
+    var response = request('GET', 'http://package.elm-lang.org/packages/' + name + '/documentation.json');
+    var body = JSON.parse(response.body);
+    fs.mkdirsSync(process.cwd() + "/docs/" + name);
+    fs.writeFileSync(process.cwd() + "/docs/" + name + "/documentation.json", JSON.stringify(response.body));
+}
+(function doStuff() {
+    console.log("Fetching docs...");
+    var response = request('GET', 'http://package.elm-lang.org/all-packages');
+    var body = JSON.parse(response.body);
+    const targets = _.flatMap(body, item => {
+        return item.versions.map(v => item.name + "/" + v);
+    }).filter(name => {
+        return !fs.existsSync(process.cwd() + "/docs/" + name);
+    });
+
+    targets.forEach(x => {
+        fetchDoc(x);
+    });
+    console.log(targets);
+
+
+})()
